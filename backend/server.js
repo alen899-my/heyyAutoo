02b9a41de13528d2fb1a7d5c641c06153a740ed9
Alen James <alenjames899@gmail.com>
2024-11-30 12:36:12 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,33 @@ require("./db/conn");
 const userRouter = require("./routes/userRoutes");
 const driverRouter = require("./routes/driverRoutes");
 const appointRouter = require("./routes/bookingRoutes");
-
 const notificationRouter = require("./routes/notificationRouter");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// Define CORS options
+const corsOptions = {
+  origin: "https://heyyyyautoooo.onrender.com/",
+  credentials: true,
+};
+
+// Use CORS with options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/driver", driverRouter);
 app.use("/api/booking", appointRouter);
 app.use("/api/notification", notificationRouter);
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (_, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-  
-})
+});
 
-
-app.listen(port, () => {});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
