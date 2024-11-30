94af479509cc08ce209085a3577ecf2936e24e6b
Alen James <alenjames899@gmail.com>
2024-11-30 12:56:44 +0530

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,15 +11,8 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Define CORS options
-const corsOptions = {
-  origin: "https://heyyyyautoooo.onrender.com/",
-  credentials: true,
-};
 
-// Use CORS with options
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/driver", driverRouter);
@@ -28,8 +21,8 @@ app.use("/api/notification", notificationRouter);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", (_, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Start the server
